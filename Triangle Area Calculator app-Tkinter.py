87c5d5592446d03ec764c9e08c7935acb9c94bc9
anokhramesh <67797819+anokhramesh@users.noreply.github.com>
2021-08-14 13:29:36 +0400

    Update Triangle Area Calculator app-Tkinter.py

--- Triangle Area Calculator app-Tkinter.py
+++ Triangle Area Calculator app-Tkinter.py
@@ -8,13 +8,13 @@ label2= Label(window,text='Enter the Base  of Triangle',fg='blue',font=('Arial',
 label2.grid(row=1,column=0,padx=5,pady=10)
 
 height= IntVar()
-breadth =IntVar()
+blenth =IntVar()
 textbox1= Entry(window,textvariable=height,fg='blue',font=('Arial',14))
 textbox1.grid(row=0,column=1)
-textbox2= Entry(window,textvariable=breadth,fg='blue',font=('Arial',14))
+textbox2= Entry(window,textvariable=blenth,fg='blue',font=('Arial',14))
 textbox2.grid(row=1,column=1)
 def findArea():
-    area=0.5*height.get()*breadth.get()
+    area=0.5*height.get()*blenth.get()
     emptylabel.config(text='The Area is:'+str(area))
 
 button1 = Button(window,command=findArea,text='Find Area',fg='blue',font=('Arial',14))
